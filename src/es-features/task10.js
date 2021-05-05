6c269add980e78f4cbdab34ba6f0e94571ee3e0b
dmytsko <dmytsko@infomedia.com.au>
2021-05-05 15:53:58 +0300

    Imported tasks without solutions

---
+++ src/es-features/task10.js
@@ -0,0 +1,55 @@
+/**
+ * Нужно переписать функции-конструкторы на классы
+ */
+
+function A(name) {
+  this.name = name;
+}
+
+A.prototype.getName = function() {
+  return this.name;
+};
+
+function B(name, age) {
+  A.call(this, name);
+  this.age = age;
+}
+
+B.prototype = Object.create(A.prototype);
+B.prototype.constructor = B;
+
+B.prototype.getName = function(text) {
+  return A.prototype.getName.call(this) + ' ' + text;
+};
+
+B.prototype.getAge = function() {
+  return this.age;
+};
+
+B.defaultUser = function() {
+  return new B('test', 0);
+};
+
+Object.defineProperty(B.prototype, 'color', {
+  get: function() {
+    return this._color;
+  },
+  set: function(color) {
+    this._color = color;
+  },
+});
+
+export function task10Old() {
+  var b = new B('Max', 12);
+  console.log(b.getName('Best')); // Max Best
+  console.log(b.getAge()); // 12
+  console.log(B.defaultUser()); // {name: 'test', age: 0}
+  b.color = 'red';
+  console.log(b.color); // red
+  return b;
+}
+
+// Перепишите функции-конструкторы A и B ниже этого комментария.
+// Названия классов должны быть NewA и NewB
+// Изменить тело функции task10Old, написав task10New, где заиспользуете
+// класс так, как использовалась функция-конструктор B в task10Old
---
+++ src/es-features/task4.js
@@ -0,0 +1,31 @@
+/**
+ * Просто преобразовать содержимое функции task4Old под современный код
+ *
+ * Пример вызова
+ * const obj = task4Old()); obj -> {x: 10, y: 10, bar: function, baztest: 'new field'}
+ * obj.bar(); -> 30
+ */
+
+export function task4Old() {
+  var x = 10;
+  var y = 20;
+
+  var obj = {
+    x: x,
+    y: y,
+    bar: function bar() {
+      return this.x + this.y;
+    },
+  };
+
+  function foo() {
+    return 'test';
+  }
+
+  obj['baz' + foo()] = 'new field';
+
+  return obj;
+}
+
+// Напишите реализацию функции task4Old на ES6+ ниже этого комментария.
+// При желании, можете использовать стрелочную функцию, вместо обычной
